377f6a4242cf05de4c089807e1c3e7a172f602c8
Isabella <isabellaj.lizarde@gmail.com>
2024-01-02 03:42:35 -0800

    Updated win/draw game functionality

--- index.js
+++ index.js
@@ -1,17 +1,50 @@
 const playerX_class = "x";
 const playerCircle_class = "circle";
-let cellElements = document.querySelectorAll("[data-cell]");
+const winning_combination = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const cellElements = document.querySelectorAll("[data-cell]");
+const gameBoardGrid = document.getElementById("game-board-grid");
+const winningMessageDisplay = document.querySelector(".winning-message");
+const quitButton = document.querySelector(".quit-button");
 let circleTurn;
 
-let handleClick = (event) => {
+startGame();
+
+quitButton.addEventListener("click", handleQuit);
+
+function handleQuit() {
+  location.reload();
+}
+
+function startGame() {
+  circleTurn = false;
+  cellElements.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHoverClass();
+}
+
+function handleClick(event) {
   const cell = event.target;
   const currentClass = circleTurn ? playerCircle_class : playerX_class;
   placeMark(cell, currentClass);
-  // check for win
-  // check for draw
-  // switch turns
-  switchTurns();
-};
+  if (checkForWin(currentClass)) {
+    endGame(false);
+  } else if (isDraw()) {
+    endGame(true);
+  } else {
+    switchTurns();
+    setBoardHoverClass();
+  }
+}
 
 let placeMark = (cell, currentClass) => {
   cell.classList.add(currentClass);
@@ -21,175 +54,45 @@ let switchTurns = () => {
   circleTurn = !circleTurn;
 };
 
-cellElements.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
+function setBoardHoverClass() {
+  gameBoardGrid.classList.remove(playerX_class);
+  gameBoardGrid.classList.remove(playerCircle_class);
+  if (circleTurn) {
+    gameBoardGrid.classList.add(playerCircle_class);
+  } else {
+    gameBoardGrid.classList.add(playerX_class);
+  }
+}
 
-// let handleClick = (event) => {
-//   let playerMark = event.target;
+function checkForWin(currentClass) {
+  return winning_combination.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentClass);
+    });
+  });
+}
 
-//   if (playerMark === playerMarkX) {
-//     playerPick.style.display = "block";
-//     playerPick.innerHTML = "You chose: X";
-//     playerMark.style.backgroundColor = "#1f3641";
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return (
+      cell.classList.contains(playerX_class) ||
+      cell.classList.contains(playerCircle_class)
+    );
+  });
+}
 
-//     let playerMarkX = new Image();
-//     playerMarkX.src = "./images/icon-x.svg";
-//     playerMarkX.classList.add("playerMarkX");
+function endGame(draw) {
+  let headerLarge = document.querySelector(".winning-message-header-large");
+  let headerSmall = document.querySelector(".winning-message-header-small");
 
-//     useStoredTarget(playerMarkX);
-//   } else {
-//     playerPick.style.display = "block";
-//     playerPick.innerHTML = "You chose: O";
-
-//     let playerMarkO = new Image();
-//     playerMarkO.src = "./images/icon-o.svg";
-//     playerMarkO.classList.add("playerMarkO");
-
-//     useStoredTarget(playerMarkO);
-//   }
-//   placeMark(cell, currentClass);
-// };
-
-// let placeMark = (cell, playerMark) => {
-//   cell.classList.add(playerMark);
-// };
-
-// let handleNewGame = () => {
-//   let gameBoard = document.getElementById("game-board");
-//   gameBoard.classList.add("active");
-
-//   let gameStartMenu = document.getElementById("game-start-menu");
-//   gameStartMenu.classList.remove("active");
-// };
-
-// let handleRestartButtonClick = (playerMark) => {
-//   markGridItems.forEach((button) => {
-//     location.reload();
-//   });
-// };
-
-// let handleHoverImageSource = (event) => {
-//   let hoverImageSource = event.target;
-
-//   if (hoverImageSource === hoverImageSourceX) {
-//     let hoverImageSourceX = new Image();
-//     hoverImageSourceX.src = "./images/icon-x-outline.svg";
-//     hoverImageSourceX.classList.add("hoverImage");
-
-//     cellElements.forEach((cell) => {
-//       cell.addEventListener("mouseover", () => {
-//         if (
-//           !cell.querySelector(".playerMarkX") &&
-//           !cell.querySelector(".playerMarkO")
-//         ) {
-//           if (!cell.contains(hoverImageSourceX)) {
-//             cell.appendChild(hoverImageSourceX);
-//           } else {
-//             hoverImageSourceX.classList.add("hoverImageSource");
-//           }
-//         }
-//       });
-//       cell.addEventListener("mouseout", () => {
-//         if (
-//           !cell.querySelector(".playerMarkX") &&
-//           !cell.querySelector(".playerMarkO")
-//         ) {
-//           hoverImageSourceX.classList.remove("hoverImageSource");
-//         }
-//       });
-//       cell.addEventListener("click", () => {
-//         if (
-//           !cell.querySelector(".playerMarkX") &&
-//           !cell.querySelector(".playerMarkO")
-//         ) {
-//           hoverImageSourceX.classList.remove("hoverImageSource");
-//           cell.removeChild(hoverImageSourceX);
-//         }
-//       });
-//     });
-//   } else {
-//     if (hoverImageSource === hoverImageSourceO) {
-//       let hoverImageSourceO = new Image();
-//       hoverImageSourceO.src = "./images/icon-o-outline.svg";
-//       hoverImageSourceO.classList.add("hoverImage");
-
-//       cellElements.forEach((cell) => {
-//         cell.addEventListener("mouseover", () => {
-//           if (
-//             !cell.querySelector(".playerMarkX") &&
-//             !cell.querySelector(".playerMarkO")
-//           ) {
-//             if (!cell.contains(hoverImageSourceO)) {
-//               cell.appendChild(hoverImageSourceO);
-//             } else {
-//               hoverImageSourceO.classList.add("hoverImageSource");
-//             }
-//           }
-//         });
-//         cell.addEventListener("mouseout", () => {
-//           if (
-//             !cell.querySelector(".playerMarkX") &&
-//             !cell.querySelector(".playerMarkO")
-//           ) {
-//             hoverImageSourceO.classList.remove("hoverImageSource");
-//           }
-//         });
-//         cell.addEventListener("click", () => {
-//           if (
-//             !cell.querySelector(".playerMarkX") &&
-//             !cell.querySelector(".playerMarkO")
-//           ) {
-//             hoverImageSourceO.classList.remove("hoverImageSource");
-//             cell.removeChild(hoverImageSourceO);
-//           }
-//         });
-//       });
-//     }
-//   }
-// };
-
-// let useStoredTarget = (playerMark) => {
-//   cellElements.forEach((cell) => {
-//     cell.addEventListener("click", () => {
-//       if (
-//         !cell.querySelector(".playerMarkX") &&
-//         !cell.querySelector(".playerMarkO")
-//       ) {
-//         cell.appendChild(playerMark.cloneNode(true));
-//       } else {
-//         alert("This box is already marked! Try another.");
-//       }
-//     });
-//   });
-// };
-
-// let swapTurns = (playerMark) => {
-//   playerMark = !playerMark;
-// };
-
-// let hoverImageSourceX = document.querySelector(".icon-x");
-// hoverImageSourceX.addEventListener("click", handleHoverImageSource);
-
-// let hoverImageSourceO = document.querySelector(".icon-o");
-// hoverImageSourceO.addEventListener("click", handleHoverImageSource);
-
-// let playerPick = document.getElementById("playerPick");
-// playerPick.style.display = "none";
-
-// let restartButton = document.querySelector("#restart-button");
-// restartButton.addEventListener("click", handleRestartButtonClick);
-
-// let newGame = document.querySelector("#new-game-solo");
-// newGame.addEventListener("click", handleNewGame);
-
-// let playerMarkX = document.querySelector(".icon-x");
-// playerMarkX.addEventListener("click", handleClick);
-
-// let playerMarkO = document.querySelector(".icon-o");
-// playerMarkO.addEventListener("click", handleClick);
-
-// let cellElements = document.querySelectorAll("[data-cell]");
-// cellElements.forEach((cell) => {
-//   cell.addEventListener("click", handleClick, { once: true });
-// });
+  if (draw) {
+    headerSmall.style.display = "none";
+    headerLarge.innerHTML = "Round Tied";
+    headerLarge.style.color = "#A8BFC9";
+    winningMessageDisplay.classList.add("show");
+  } else {
+    headerSmall.innerHTML = `${circleTurn ? "You won!" : "Oh no, you lost..."}`;
+    headerLarge.innerHTML = `${circleTurn ? "O" : "X"} takes the win!`;
+    winningMessageDisplay.classList.add("show");
+  }
+}
