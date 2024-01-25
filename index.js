4c242d7384f95da68b9e9b653a3cc6a15c873c6f
Isabella <isabellaj.lizarde@gmail.com>
2024-01-24 19:10:55 -0800

    Completed functionality for mark hover effect

--- index.js
+++ index.js
@@ -12,12 +12,7 @@ const selectBox = document.querySelector(".game-start-menu"),
   newGameButton = document.querySelector("#new-game-solo");
 newGameButton.addEventListener("click", startGame);
 let playerButtonClicked = false;
-
-playerButton.forEach((button) => {
-  button.addEventListener("click", function () {
-    playerButtonClicked = true;
-  });
-});
+runAi = true;
 
 window.onload = () => {
   //make sure all boxes in board are clickable
@@ -25,15 +20,17 @@ window.onload = () => {
     cellElements[i].setAttribute("onclick", "clickedBox(this)");
   }
   playerDisplay.style.display = "none";
-  // element.removeEventListener("mouseover", handleMouseOver_Circle);
-  // element.removeEventListener("mouseover", handleMouseOver_X);
 };
+
+playerButton.forEach((button) => {
+  button.addEventListener("click", function () {
+    playerButtonClicked = true;
+  });
+});
 
 selectPlayerX.onclick = () => {
   playerDisplay.innerHTML = "You chose X.";
   playerDisplay.style.display = "block";
-  // handleMouseOver();
-  // playerSign = "x";
 };
 
 selectPlayerO.onclick = () => {
@@ -43,10 +40,26 @@ selectPlayerO.onclick = () => {
     "class",
     "third-container pick-players players active player"
   );
-  // handleMouseOver();
-  // playerSign = "circle";
   aiPlayer();
 };
+
+// Define the event listener function
+function handleMouseOver() {
+  if (players.classList.contains("active")) {
+    this.style.backgroundImage = "url('./images/icon-o-outline.svg')";
+    this.style.backgroundRepeat = "no-repeat";
+    this.style.backgroundPosition = "center";
+  } else if ((playerSign = "x")) {
+    this.style.backgroundImage = "url('./images/icon-x-outline.svg')";
+    this.style.backgroundRepeat = "no-repeat";
+    this.style.backgroundPosition = "center";
+  }
+}
+
+// Attach the event listener to each cell element
+cellElements.forEach((element) => {
+  element.addEventListener("mouseover", handleMouseOver);
+});
 
 function startGame() {
   if (!playerButtonClicked) {
@@ -57,11 +70,6 @@ function startGame() {
   }
 }
 
-let iconX = document.querySelector(".icon-x");
-let iconO = document.querySelector(".icon-o");
-playerSign = "x";
-runAi = true;
-
 // user interaction with board
 function clickedBox(element) {
   if (players.classList.contains("players")) {
@@ -69,19 +77,13 @@ function clickedBox(element) {
     element.classList.add("circle");
     players.classList.remove("active");
     element.setAttribute("id", playerSign);
-    // cellElements.forEach((element) =>
-    //   element.removeEventListener("mouseover", handleMouseOver)
-    // );
-    // element.removeEventListener("mouseover", handleMouseOver);
+    element.removeEventListener("mouseover", handleMouseOver);
   } else {
     playerSign = "x";
     element.classList.add("x");
     players.classList.add("active");
     element.setAttribute("id", playerSign);
-    // cellElements.forEach((element) =>
-    //   element.removeEventListener("mouseover", handleMouseOver)
-    // );
-    // element.removeEventListener("mouseover", handleMouseOver);
+    element.removeEventListener("mouseover", handleMouseOver);
   }
   selectWinner();
   element.style.pointerEvents = "none";
@@ -94,59 +96,13 @@ function clickedBox(element) {
   }, randomTimeDelay);
 }
 
-function handleMouseOver() {
-  if (iconO) {
-    console.log(playerSign);
-    this.style.backgroundImage = "url('./images/icon-o-outline.svg')";
-  } else if (iconX) {
-    console.log(playerSign);
-    this.style.backgroundImage = "url('./images/icon-x-outline.svg')";
-  }
-  this.style.backgroundRepeat = "no-repeat";
-  this.style.backgroundPosition = "center";
-}
-
 function handleMouseOut() {
   this.style = "none";
 }
 
 cellElements.forEach((element) =>
-  element.addEventListener("mouseover", handleMouseOver)
-);
-cellElements.forEach((element) =>
   element.addEventListener("mouseout", handleMouseOut)
 );
-
-// function handleHover_Circle() {
-//   if ((playerSign = "circle")) {
-//     this.style.backgroundImage = "url('./images/icon-o-outline.svg')";
-//     this.style.backgroundRepeat = "no-repeat";
-//     this.style.backgroundPosition = "center";
-
-//     this.addEventListener("mouseout", () => {
-//       this.style = "none";
-//     });
-//   }
-// }
-
-// function handleHover_X() {
-//   if ((playerSign = "x")) {
-//     this.style.backgroundImage = "url('./images/icon-x-outline.svg')";
-//     this.style.backgroundRepeat = "no-repeat";
-//     this.style.backgroundPosition = "center";
-
-//     this.addEventListener("mouseout", () => {
-//       this.style = "none";
-//     });
-//   }
-// }
-
-// cellElements.forEach((element) => {
-//   element.addEventListener(
-//     "mouseover",
-//     (playerSign = "circle" ? handleHover_Circle : handleHover_X)
-//   );
-// });
 
 // computer interaction with board
 function aiPlayer() {
@@ -249,188 +205,8 @@ function selectWinner() {
   }
 }
 
-// const playerX_class = "x";
-// const playerCircle_class = "circle";
-// const winning_combination = [
-//   [0, 1, 2],
-//   [3, 4, 5],
-//   [6, 7, 8],
-//   [0, 3, 6],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [0, 4, 8],
-//   [2, 4, 6],
-// ];
-// const cellElements = document.querySelectorAll("[data-cell]");
-// const pickPlayers = document.querySelector(".pick-players");
-// const newGameButton = document.querySelector("#new-game-solo");
-// const gameMenu = document.querySelector(".game-start-menu");
-// const gameBoard = document.querySelector(".game-board");
-// const gameBoardGrid = document.getElementById("game-board-grid");
-// const gameEndMessageDisplay = document.getElementById("game-end-message");
-// const quitButton = document.querySelector(".quit-button");
-// const restartButton = document.querySelector(".restart-button");
-// const turnButton = document.querySelector(".turn-button");
-// const playerOne = document.getElementById("player-one");
-// let playerDisplay = document.getElementById("playerDisplay");
-// const iconX = document.querySelector(".icon-x");
-// const iconCircle = document.querySelector(".icon-o");
-// let aiPlayer = true;
-// let playerCircle;
-// let playerSign;
-
-// startGame();
-
-// // NEXT create ai player with minimax algorithm
-// // AFTER work on scores to update and show wins/ties/loses
-
-// quitButton.addEventListener("click", handleQuit);
-// restartButton.addEventListener("click", handleQuit);
-// turnButton.addEventListener("click", handleTurnButton);
-// newGameButton.addEventListener("click", handleNewGame);
-// iconX.addEventListener("click", playerSelect_X);
-// playerDisplay.classList.add("hide");
-// iconCircle.addEventListener("click", function (event) {
-//   storePlayerSelect_Circle(event.target);
-//   usePlayerSelect_Circle();
-// });
-
-// function storePlayerSelect_Circle(target) {
-//   playerCircle = target;
-//   playerCircle.classList.add(playerCircle_class);
-// }
-
-// function usePlayerSelect_Circle() {
-//   if (playerCircle) {
-//     playerDisplay.style.display = "block";
-//     playerDisplay.innerHTML = "You chose O.";
-//     // gameBoardGrid.classList.add(playerCircle_class);
-//     // gameBoardGrid.classList.remove(playerX_class);
-//     iconCircle.classList.add("light-background");
-//     iconX.classList.remove("light-background");
-//     // aiPlayer = iconX;
-//     // console.log(aiPlayer);
-//     // aiPlayerMove();
-//   } else {
-//     console.log("playerCircle not found");
-//   }
-// }
-
-// function playerSelect_X(event) {
-//   let playerX = event.target;
-//   if (playerX) {
-//     playerDisplay.style.display = "block";
-//     playerDisplay.innerHTML = "You chose X.";
-//     iconX.classList.add("light-background");
-//     iconCircle.classList.remove("light-background");
-//     // aiPlayer = iconCircle;
-//     // aiPlayerMove();
-//   }
-// }
-// function timeDelay() {
-//   let randomTimeDelay = (Math.random() * 1000 + 200).toFixed();
-//   setTimeout(() => {
-//     aiPlayerMove(aiPlayer);
-//   }, randomTimeDelay);
-// }
-
-// function aiPlayerMove() {
-//   let array = [];
-//   if (aiPlayer) {
-//     for (let i = 0; i < cellElements.length; i++) {
-//       if (cellElements[i].childElementCount === 0) {
-//         array.push(i);
-//       }
-//     }
-//     let randomBox = array[Math.floor(Math.random() * array.length)];
-//     console.log(randomBox);
-//     if (array.length > 0) {
-//       // console.log("test");
-//       return randomBox;
-//     } else {
-//       // cellElements[randomBox].classList.add("circle");
-//       // }
-//       checkForWin();
-//     }
-//   }
-// }
-
-// function handleQuit() {
-//   location.reload();
-// }
-
-// function handleNewGame() {
-//   aiPlayer === true;
-//   if (playerDisplay.style.display === "block") {
-//     gameMenu.classList.add("hide");
-//     gameBoard.classList.remove("hide");
-//   } else {
-//     alert("Player 1 must pick a mark.");
-//   }
-// }
-
-// function startGame() {
-//   cellElements.forEach((cell) => {
-//     cell.addEventListener("click", handleClick, { once: true });
-//   });
-//   setBoardHoverClass();
-//   handleTurnButton();
-// }
-
-// function handleClick(event) {
-//   const cell = event.target;
-//   const currentClass = playerCircle ? playerCircle_class : playerX_class;
-//   aiPlayerMove();
-//   placeMark(cell, currentClass);
-//   if (checkForWin(currentClass)) {
-//     endGame(false);
-//   } else if (isDraw()) {
-//     endGame(true);
-//   } else {
-//     setBoardHoverClass();
-//     handleTurnButton();
-//     setPlayers();
-//   }
-// }
-
 // function handleTurnButton() {
 //   turnButton.innerHTML = playerCircle ? "O turn" : "X turn";
-// }
-
-// function setPlayers() {
-//   if (playerCircle) {
-//     playerOne.innerHTML = "O (You)";
-//   } else {
-//     playerOne.innerHTML = "X (You)";
-//   }
-// }
-
-// let placeMark = (cell, currentClass) => {
-//   if (!cell.hasChildNodes()) {
-//     cell.classList.add(currentClass);
-//   }
-// };
-
-// // let switchTurns = () => {
-// //   playerCircle = !playerCircle;
-// // };
-
-// function setBoardHoverClass() {
-//   gameBoardGrid.classList.remove(playerX_class);
-//   gameBoardGrid.classList.remove(playerCircle_class);
-//   if (playerCircle) {
-//     gameBoardGrid.classList.add(playerCircle_class);
-//   } else {
-//     gameBoardGrid.classList.add(playerX_class);
-//   }
-// }
-
-// function checkForWin(currentClass) {
-//   return winning_combination.some((combination) => {
-//     return combination.every((index) => {
-//       return cellElements[index].classList.contains(currentClass);
-//     });
-//   });
 // }
 
 // function isDraw() {
